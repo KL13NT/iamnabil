765a465f632e3f7936a222f449282cb528fad007
Nabil <nabil.tharwat@outlook.com>
2020-02-16 07:34:53 +0200

    Improved header modularity
    Signed-off-by: Nabil <nabil.tharwat@outlook.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,8 @@ import React, { Fragment } from 'react'
 
 import Social from '../components/Social'
 
-
-export default ({ tiny }) => tiny?
-	<div className="o-header u-justify">
+const MainHeader = () =>
+	<>
 		<h1 className="u-center">I'm Nabil Tharwat —</h1>
 		<h3 className="u-center">Front-End Engineer, Blogger, Mentor</h3>
 		<p>
@@ -12,23 +11,15 @@ export default ({ tiny }) => tiny?
 			there. After graduating high school I embarked on a mission to become
 			a software engineer. I'm the author of Ally Reads! and DOCX.
 		</p>
-		<Social/>
-	</div>
-	: <div className="o-header u-justify">
-		<h1 className="u-center">I'm Nabil Tharwat —</h1>
-		<h3 className="u-center">Front-End Engineer, Blogger, Mentor</h3>
-		<p>
-			I'm a software engineer, and occasionally a blogger and mentor. I was born in Cairo, Egypt and grew up in
-			there. After graduating High School I embarked on a mission to become
-			a software engineer. I'm the creator of Ally Reads! and DOCX.
-		</p>
-		<br />
+	</>
+
+const RestHeader = () =>
+	<>
 		<p>
 			I love contributing to open source software, and I'm also a performance addict. Whenever there's a tiny
 			chance to beneficially improve the optimisation of my code, I will gladly do it.
 			Accessibility and performance are part of my eternal quest.
 		</p>
-		<br />
 		<p>
 			I recently started an online student community that aims to improve
 			students' lives by providing the help and support needed. We also
@@ -36,5 +27,14 @@ export default ({ tiny }) => tiny?
 			Everyone is welcome! Be sure to check out our Discord server if you're an Egyptian/Arab student.
 			<a href="https://discord.gg/xrGAnTg">Click here to check it out!</a>
 		</p>
+	</>
+
+
+export default ({ tiny }) =>
+	<div className="o-header u-justify">
+		<MainHeader/>
+		{
+			tiny? null: <RestHeader/>
+		}
 		<Social/>
 	</div>
