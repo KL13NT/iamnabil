8d206894377fc92eadb8b384c6f249b14870d4ca
Nabil <nabil.tharwat@outlook.com>
2020-05-28 04:34:51 +0200

    [bug] replaced iamnabil.me with iamnabil.netlify.app
    Signed-off-by: Nabil <nabil.tharwat@outlook.com>

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,41 +1,41 @@
 import React, { Fragment } from 'react'
 import { Helmet } from 'react-helmet'
 
-const hostname = `https://iamnabil.me`
-const defaultOGImagePath = `/icons/icon-512x512.png`
+const hostname = 'https://iamnabil.netlify.app'
+const defaultOGImagePath = '/icons/icon-512x512.png'
 
 
 function SEO ({ title, description, path, ogImageName, ogImageExtension }) {
-  const ogImageUrl = ogImageName? `${hostname}/${ogImageName}.${ogImageExtension}`: defaultOGImagePath
-  const url = `${hostname}${path? path: null}`
-  
-  return (
-    <Helmet>
-      <title>{title}</title>
-      <meta property="og:title" content={title} />
-      <meta property="og:type" content="website" />
-      <meta name="description" content={description} />
-      <meta property="og:description" content={description} />
-      <meta property="og:url" content={url} />
-      <meta property="og:site_name" content="Nabil Tharwat" />
-      <meta property="twitter:title" content={title} />
-      <meta property="twitter:description" content={description} />
-      <meta property="twitter:card" content="summary" />
-      <meta property="twitter:creator" content="@Nabil_Tharwat" />
-      <meta property="twitter:site" content="@Nabil_Tharwat16"/>
-      <meta property="og:image" content={ogImageUrl}/>
-      <meta property="og:image:type" content={`image/jpeg`}/>
-      <meta property="og:image:url" content={ogImageUrl}/>
-      <meta property="og:image:secure_url" content={ogImageUrl}/>
-      <meta property="twitter:image" content={ogImageUrl}/>
-      {/* {
-        ogImageName 
+	const ogImageUrl = ogImageName? `${hostname}/${ogImageName}.${ogImageExtension}`: defaultOGImagePath
+	const url = `${hostname}${path? path: null}`
+
+	return (
+		<Helmet>
+			<title>{ title }</title>
+			<meta content={ title } property='og:title' />
+			<meta content='website' property='og:type' />
+			<meta content={ description } name='description' />
+			<meta content={ description } property='og:description' />
+			<meta content={ url } property='og:url' />
+			<meta content='Nabil Tharwat' property='og:site_name' />
+			<meta content={ title } property='twitter:title' />
+			<meta content={ description } property='twitter:description' />
+			<meta content='summary' property='twitter:card' />
+			<meta content='@Nabil_Tharwat' property='twitter:creator' />
+			<meta content='@Nabil_Tharwat16' property='twitter:site'/>
+			<meta content={ ogImageUrl } property='og:image'/>
+			<meta content={ 'image/jpeg' } property='og:image:type'/>
+			<meta content={ ogImageUrl } property='og:image:url'/>
+			<meta content={ ogImageUrl } property='og:image:secure_url'/>
+			<meta content={ ogImageUrl } property='twitter:image'/>
+			{ /* {
+        ogImageName
           ? <SEOImages ogImageName={ogImageName} ogImageExtension={ogImageExtension}/>
           : null
-      } */}
-      <html lang="en-GB"/>
-    </Helmet>
-  )
+      } */ }
+			<html lang='en-GB'/>
+		</Helmet>
+	)
 }
 
 
