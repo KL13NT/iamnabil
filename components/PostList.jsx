906a95d0f761f638eb9dc485b732aea4424c0b22
Nabil <nabil.tharwat@outlook.com>
2020-12-21 17:52:53 +0200

    fix: netlify identity loaded on all pages

--- components/PostList.jsx
+++ components/PostList.jsx
@@ -5,10 +5,10 @@ import { Post } from './Post'
 export const PostList = ({ posts }) => (
 	<ul className='mt-12'>
 		{posts.map(post => (
-			<>
+			<React.Fragment key={post.frontmatter.path}>
 				<Post {...post} key={post.frontmatter.path} />
 				<hr />
-			</>
+			</React.Fragment>
 		))}
 	</ul>
 )
