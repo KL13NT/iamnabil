71ebf943d74c81bcdee6c400924fa42186400347
Nabil Tharwat <nabil.tharwat@outlook.com>
2022-12-02 16:29:35 +0200

    feat: article newsletter
    Signed-off-by: Nabil Tharwat <nabil.tharwat@outlook.com>

--- components/Article.jsx
+++ components/Article.jsx
@@ -4,11 +4,16 @@ import React from 'react'
 import ReactMarkdown from 'react-markdown/with-html'
 
 import { LightAsync as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { shadesOfPurple } from 'react-syntax-highlighter/dist/cjs/styles/hljs'
+import { atomOneDarkReasonable } from 'react-syntax-highlighter/dist/cjs/styles/hljs'
 
 import SEO from './SEO'
 import { PostList } from './PostList'
-import { getTwitterPath, getTwitterSearchPath } from '../utils'
+import {
+	getArticleGithubPath,
+	getTwitterPath,
+	getTwitterSearchPath
+} from '../utils'
+import Newsletter from './Newsletter'
 
 const renderers = {
 	paragraph: ({ children }) => <p dir='auto'>{children}</p>,
@@ -19,7 +24,7 @@ const renderers = {
 	blockquote: ({ children }) => <blockquote dir='auto'>{children}</blockquote>,
 	code: ({ language, value }) => (
 		<SyntaxHighlighter
-			style={shadesOfPurple}
+			style={atomOneDarkReasonable}
 			language={language}
 			showLineNumbers
 		>
@@ -28,7 +33,7 @@ const renderers = {
 	),
 	pre: ({ language, value }) => (
 		<SyntaxHighlighter
-			style={shadesOfPurple}
+			style={atomOneDarkReasonable}
 			language={language}
 			showLineNumbers
 		>
@@ -49,12 +54,21 @@ const textLanguageMapping = {
 	discuss: {
 		ar: 'ناقش هذه المقالة على تويتر',
 		en: 'Discuss this article on Twitter'
+	},
+	github: {
+		ar: 'عدل هذه المقالة على جيتهب',
+		en: 'Edit on GitHub'
 	}
 }
 
-function ArticleHeaderLink({ href, title, src }) {
+function ArticleHeaderLink({ href, title, src, text }) {
 	return (
-		<a href={href} title={title} className='bg-transparent mx-2'>
+		<a
+			href={href}
+			title={title}
+			className='bg-transparent mx-2 flex items-center w-full justify-between'
+		>
+			{text ? <span className='block mie-2'>{text}</span> : null}
 			<img alt='' role='presentation' src={src} className='h-8' />
 		</a>
 	)
@@ -63,27 +77,13 @@ function ArticleHeaderLink({ href, title, src }) {
 export default function Article({ html, frontmatter, path, lang, related }) {
 	const { date, title, translation, cover } = frontmatter
 
-	const articleHeaderLinksMarkup = (
-		<p dir={lang === 'ar' ? 'ltr' : 'rtl'} className='flex'>
-			{translation ? (
-				<ArticleHeaderLink
-					href={translation}
-					title={textLanguageMapping.translation[lang]}
-					src='/language-svgrepo-com.svg'
-				/>
-			) : null}
-			<ArticleHeaderLink
-				href={getTwitterPath(title, path)}
-				title={textLanguageMapping.tweet[lang]}
-				src='/twitter-tweet.svg'
-			/>
-			<ArticleHeaderLink
-				href={getTwitterSearchPath(path)}
-				title={textLanguageMapping.discuss[lang]}
-				src='/twitter-reply.svg'
-			/>
-		</p>
-	)
+	const translationMarkup = translation ? (
+		<ArticleHeaderLink
+			href={translation}
+			title={textLanguageMapping.translation[lang]}
+			src='/language-svgrepo-com.svg'
+		/>
+	) : null
 
 	return (
 		<div className='mx-auto mb-20'>
@@ -96,7 +96,24 @@ export default function Article({ html, frontmatter, path, lang, related }) {
 				<div className='w-full'>
 					<div className='flex justify-between w-full'>
 						<p>{date}</p>
-						{articleHeaderLinksMarkup}
+						<p dir={lang === 'ar' ? 'ltr' : 'rtl'} className='flex'>
+							<ArticleHeaderLink
+								href={getArticleGithubPath(path)}
+								title={textLanguageMapping.github[lang]}
+								src='/github.svg'
+							/>
+							<ArticleHeaderLink
+								href={getTwitterPath(title, path)}
+								title={textLanguageMapping.tweet[lang]}
+								src='/twitter-tweet.svg'
+							/>
+							<ArticleHeaderLink
+								href={getTwitterSearchPath(path)}
+								title={textLanguageMapping.discuss[lang]}
+								src='/twitter-reply.svg'
+							/>
+							{translationMarkup}
+						</p>
 					</div>
 					<h1 className='mt-4 mb-4'>{title}</h1>
 				</div>
@@ -116,23 +133,49 @@ export default function Article({ html, frontmatter, path, lang, related }) {
 				<ReactMarkdown renderers={renderers} allowDangerousHtml>
 					{html}
 				</ReactMarkdown>
+
+				<div className='mx-auto mt-10 lg:mt-20 flex flex-col sm:flex-row justify-between w-full'>
+					<div className='flex flex-col sm:flex-row w-full'>
+						<ArticleHeaderLink
+							text={textLanguageMapping.tweet[lang]}
+							href={getTwitterPath(title, path)}
+							title={textLanguageMapping.tweet[lang]}
+							src='/twitter-tweet.svg'
+						/>
+						<ArticleHeaderLink
+							text={textLanguageMapping.discuss[lang]}
+							href={getTwitterSearchPath(path)}
+							title={textLanguageMapping.discuss[lang]}
+							src='/twitter-reply.svg'
+						/>
+					</div>
+					<ArticleHeaderLink
+						text={textLanguageMapping.github[lang]}
+						href={getArticleGithubPath(path)}
+						title={textLanguageMapping.github[lang]}
+						src='/github.svg'
+					/>
+				</div>
+				<hr className='w-full mt-5 mx-auto' />
+				<div className='mx-auto mt-10 lg:mt-20'>
+					<h3>Written by Nabil Tharwat</h3>
+					<p>
+						Nabil Tharwat is a software engineer and mentor who's super in love
+						with all things accessibility and performance. He's host of The
+						Weekly Noob podcast and his content has reached thousands of people
+						around the world.
+					</p>
+					<p className='mt-4'>
+						<a href='/about'>Learn more about Nabil.</a>
+					</p>
+					<Newsletter />
+				</div>
 			</article>
-			<hr className='mt-10 lg:mt-20 w-3/5 mx-auto' />
-			<div className='mx-auto mt-10 lg:mt-20 text-content'>
-				<h3>Written by Nabil Tharwat</h3>
-				<p>
-					Nabil Tharwat is a software engineer and mentor who's super in love
-					with all things accessibility and performance. He's host of The Weekly
-					Noob podcast and his content has reached thousands of people around
-					the world.
-				</p>
-				<p className='mt-4'>
-					<a href='/about'>Learn more about Nabil.</a>
-				</p>
-			</div>
-			<div className='mx-auto mt-20 lg:mt-40'>
+			<div className='mx-auto mt-20'>
 				<h3>If you found this article helpful, you will love these as well.</h3>
-				<PostList posts={related} lang={lang} />
+				<div className='mt-6'>
+					<PostList posts={related} lang={lang} />
+				</div>
 			</div>
 		</div>
 	)
