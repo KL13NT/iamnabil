c422a01224bc7a641e9113f47d8bb0551e07140f
Nabil Tharwat <nabil.tharwat@outlook.com>
2024-09-29 06:07:20 +0300

    add sponsors page
    Signed-off-by: Nabil Tharwat <nabil.tharwat@outlook.com>

---
+++ components/BuyMeACuppa.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BuyMeACuppa() {
+	return (
+		<a href='https://www.buymeacoffee.com/kl13nt'>
+			<img src='https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=☕&slug=kl13nt&button_colour=FFDD00&font_colour=000000&font_family=Bree&outline_colour=000000&coffee_colour=ffffff' />
+		</a>
+	)
+}
+
+export default BuyMeACuppa
