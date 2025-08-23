6016907c4204c827856f84d4b8fb84bdb5564668
Nabil Tharwat <nabil.tharwat@outlook.com>
2025-08-23 10:18:32 +0300

    fixes for smaller screens
    Signed-off-by: Nabil Tharwat <nabil.tharwat@outlook.com>

--- src/messsages.ts
+++ src/messsages.ts
@@ -20,7 +20,7 @@ export const textLanguageMapping = {
 		en: 'Written by Nabil Tharwat'
 	},
 	articleFooterBioBody: {
-		ar: 'نبيل مهندس برمجيات ومرشد، يعشق بشدة كل ما يتعلق بإمكانية الوصول والأداء. وهو مقدم The Weekly Noob بودكاست، وقد وصلت محتوياته إلى آلاف الأشخاص حول العالم.',
+		ar: 'نبيل مهندس برمجيات ومينتور، يعشق بشدة كل ما يتعلق بإمكانية الوصول والأداء. وهو مقدم The Weekly Noob بودكاست، وقد وصلت محتوياته إلى آلاف الأشخاص حول العالم.',
 		en: `Nabil Tharwat is a software engineer and mentor who's super in love with all
 		things accessibility and performance. He's host of The Weekly Noob podcast
 		and his content has reached thousands of people around the world.`
