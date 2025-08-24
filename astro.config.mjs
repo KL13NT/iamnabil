c57bd8fe2dd85c66d59d640fb83302a749a89586
Nabil Tharwat <nabil.tharwat@outlook.com>
2025-08-24 08:38:27 +0300

    prefetch all page links
    Signed-off-by: Nabil Tharwat <nabil.tharwat@outlook.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,7 +24,7 @@ export default defineConfig({
 	integrations: [mdx(), react(), sitemap()],
 	site: 'https://nabiltharwat.com',
 	prefetch: {
-		defaultStrategy: 'hover',
+		defaultStrategy: 'viewport',
 		prefetchAll: true
 	},
 	image: {
